cda863a2ddb4692940b4fbf9c10fd94790f08765
Jonathan Makela <jonathan.j.makela@gmail.com>
2023-09-15 09:34:07 -0700

    Add code to reboot skyalert if it is not responsive

--- skyalert-logger.py
+++ skyalert-logger.py
@@ -2,20 +2,38 @@ import requests
 from time import sleep
 from datetime import datetime
 
+from powercontrol import PowerControl
 from get_IP import get_IP_from_MAC
+from config import config
 
 ## JJM: MIGHT WANT THIS TO BE COMMAND LINE INPUTS
-#address='http://192.168.1.23:81'
-skyAlertMAC = '2c:f7:f1:b8:10:73'
+#address='http://192.168.1.126:81'
+skyAlertMAC = config['skyAlertMAC']
 logfolder = './skyalert-logs/'
 logfolder = '/home/airglow/airglow/skyalert-logger/skyalert-logs/'
-site = 'blo'
+site = config['site']
 index = 0
 
+powerControl = PowerControl(config['powerSwitchAddress'], config['powerSwitchUser'], config['powerSwitchPassword'])
+    
 # get the SkyAlert IP address from the MAC address using ARP
 SkyAlert_IP = get_IP_from_MAC(skyAlertMAC)
-address = 'http://' + SkyAlert_IP + ':81'
-print("Found skyalert on " + address)
+if SkyAlert_IP is None:
+    # Try rebooting
+    print("Cannot find skyalert; rebooting")
+    powerControl.turnOff(config['CloudSensorPowerPort'])
+    sleep(5)
+    powerControl.turnOn(config['CloudSensorPowerPort'])
+    sleep(60)
+    SkyAlert_IP = get_IP_from_MAC(config['skyAlertMAC'])
+    if SkyAlert_IP is not None:
+        address = 'http://' + SkyAlert_IP + ':81'
+        print('Found SkyAlert at %s' % SkyAlert_IP)
+    else:
+        print('Could not find SkyAlert after power cycle')
+else:
+    address = 'http://' + SkyAlert_IP + ':81'
+    print("Found SkyAlert on " + address)
 
 while 1:
     count = 5
@@ -31,13 +49,27 @@ while 1:
                 # Try to find the SkyAlert (maybe add reboot capability?
                 print("Trying to find the IP of skyalert")
                 SkyAlert_IP = get_IP_from_MAC(skyAlertMAC)
-                address = 'http://' + SkyAlert_IP + ':81'
-                print("Found skyalert on " + address)
+                if SkyAlert_IP is None:
+                    # Try rebooting
+                    print("Cannot find skyalert; rebooting")
+                    powerControl.turnOff(config['CloudSensorPowerPort'])
+                    sleep(5)
+                    powerControl.turnOn(config['CloudSensorPowerPort'])
+                    sleep(60)
+                    SkyAlert_IP = get_IP_from_MAC(config['skyAlertMAC'])
+                    if SkyAlert_IP is not None:
+                        address = 'http://' + SkyAlert_IP + ':81'
+                        print('Found SkyAlert at %s' % SkyAlert_IP)
+                    else:
+                        print('Could not find SkyAlert after power cycle')
+                else:
+                    address = 'http://' + SkyAlert_IP + ':81'
+                    print("Found skyalert on " + address)
             sleep(5)
 
     print(arr)
     now = datetime.now()
-    with open(logfolder + 'Cloud_%s_' % site + now.strftime('%Y%m%d.txt'), 'a+') as f:
+    with open(logfolder + 'Cloud_%s_' % site.lower() + now.strftime('%Y%m%d.txt'), 'a+') as f:
         timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
         f.write(f'{timestamp} {arr}\n')
     sleep(20)

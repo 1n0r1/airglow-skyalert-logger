340ad629733a8db3001c3025a3a48384ce3f79aa
Khanh Nguyen <khanhn2@illinois.edu>
2023-09-15 14:52:53 -0500

    Force a nmap ahead of loading ARP

--- get_IP.py
+++ get_IP.py
@@ -2,6 +2,10 @@ import os
 # Following https://stackoverflow.com/a/59559717
 
 def get_IP_from_MAC(MAC):
+
+    # Run nmap to populate the ARP table
+    os.popen('nmap -sP 192.168.1.0/24')
+
     for device in os.popen('arp -a'):
         # example output: xxxx (192.168.1.254) at xx:xx:xx:xx:xx:xx [ether] on wlp..
         _, ip, _, phy, _ = device.split(maxsplit=4)
@@ -11,5 +15,18 @@ def get_IP_from_MAC(MAC):
         if phy == MAC:
             return ip
 
-    print('Could not find %s' % MAC)
+    print('Could not find %s. Trying again.' % MAC)
+
+    for device in os.popen('arp -a'):
+        # example output: xxxx (192.168.1.254) at xx:xx:xx:xx:xx:xx [ether] on wlp..
+        _, ip, _, phy, _ = device.split(maxsplit=4)
+        # remove the paranthesis around the ip address
+        ip = ip.strip('()')
+
+        if phy == MAC:
+            return ip
+
+    print('Still could not find %s. Quitting' % MAC)
+
     return None
+
